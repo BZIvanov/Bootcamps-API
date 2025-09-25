a2cf8279d5d859a690f8c1823b27cef8b4588039
BZIvanov <biser.z.ivanov@gmail.com>
2025-09-25 19:09:04 +0300

    update mongoose and nodemon packages

--- startup/db.js
+++ startup/db.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 const log = require('../utils/log');
 
-mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then((connection) => {
-    log(
-      'info',
-      `DB connection successful and ready for ${connection.connections[0].host}`
-    );
-  });
+mongoose.connect(process.env.DB_URI).then((connection) => {
+  log(
+    'info',
+    `DB connection successful and ready for ${connection.connections[0].host}`
+  );
+});
 // don't catch errors here, so the error-handling can catch the error and also shut down the server in case of unhandledRejection
